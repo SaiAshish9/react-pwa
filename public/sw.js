aba8df9585f3e110d0ed1d3f4f30117d7775b41e
SaiAshish9 <saiashish3760@gmail.com>
2020-11-08 19:39:03 +0530

    offline caching updated

--- public/sw.js
+++ public/sw.js
@@ -7,16 +7,24 @@ this.addEventListener("install", (e) => {
         "/static/js/0.chunk.js",
         "/static/js/bundle.js",
         "/index.html",
-        "/"
+        "/",
+        "/users",
       ]);
     })
   );
 });
 
-this.addEventListener("fetch",(e)=>{
+this.addEventListener("fetch", (e) => {
+  if (!navigator.onLine) {
     e.respondWith(
-        caches.match(e.request).then(res=>{
-            if(res) return res
-        })
-    )
-})
+      caches.match(e.request).then((res) => {
+        if (res) return res;
+        let  requestUrl = e.request.clone()
+        fetch(requestUrl)
+      })
+    );
+  }
+});
+
+
+//  npm i web push
--- src/Users.js
+++ src/Users.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
 
 const Users = () => {
-    return (
-        <div>
-            users
-        </div>
-    )
-}
+  const [data, setData] = useState([]);
+  const [mode, setMode] = useState("online");
+  useEffect(() => {
+    let url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+      .then((res) => {
+        res.json().then((result) => {
+          setData(result);
+          localStorage.setItem("users", JSON.stringify(result));
+        });
+      })
+      .catch((e) => {
+        setMode("offline");
+        let collection = localStorage.getItem("users");
+        setData(JSON.parse(collection));
+      });
+  }, []);
+  return (
+    <div>
+      <div>
+        {mode === "offline" && <div class="alert alert-warning">Offline</div>}
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 &&
+            data.map((i, k) => (
+              <tr key={k}>
+                <td>{i.id}</td>
+                <td>{i.username}</td>
+                <td>{i.email}</td>
+                <td>{i.address.street}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>{" "}
+    </div>
+  );
+};
 
-export default Users
+export default Users;
